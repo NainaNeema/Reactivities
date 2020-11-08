b4229142c1225ea52b5505feb8679079f241ca9d
NainaNeema <nainaneema20@gmail.com>
2020-11-08 11:12:20 +0530

    Initial Commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get;set; }
+
+        protected override void OnModelCreating (ModelBuilder builder){
+            
+            builder.Entity<Value>().HasData(
+                new Value{Id =101, Name ="Laxmi"},
+                new Value{Id =102, Name ="Kaali"},
+                new Value{Id =103, Name ="Saraswati"}
+            );
+        }
+    }
+}
